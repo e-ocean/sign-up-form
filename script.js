a3000bb0ca24511c9eff4451f052589c8a56a52b
e-ocean <qxxsnd@gmail.com>
2023-12-27 18:53:21 -0500

    Expiremented with media queries, changed flex direction of body to column for mobile users

---
+++ script.js
@@ -0,0 +1,63 @@
+
+const passwordsEl = document.querySelector("body > div.right-side > div > form > fieldset:nth-child(3) > div")
+const password1 = document.querySelector("#password")
+const password2 = document.querySelector("#password2")
+const passwords = document.querySelectorAll(".password-input")
+const numberEl = document.querySelector("#tel")
+console.log(numberEl)
+
+
+//to style invalid inputs only after user interacts with them
+document.querySelectorAll('input, select, textarea').forEach(function(el) {
+    el.addEventListener('input', function() {
+        this.classList.add('touched');
+    });
+});
+
+
+//function to match password fields
+
+function matchPasswords(){
+    if (password1.value === password2.value){
+        passwordsEl.classList.remove("error")
+    }
+    else{
+        passwordsEl.classList.add("error")
+    }
+}
+
+passwords.forEach(function (input){
+    input.addEventListener('input', matchPasswords)
+})
+
+numberEl.addEventListener('input', (event) =>{
+     if (numberEl.validity.patternMismatch){
+        numberEl.setCustomValidity("Please enter a valid phone-number")
+    } 
+    else {
+        numberEl.setCustomValidity('')
+    }
+    
+})
+
+
+// email.addEventListener("input", (event) => {
+//     if (email.validity.typeMismatch) {
+//       email.setCustomValidity("I am expecting an email address!");
+//     } else {
+//       email.setCustomValidity("");
+//     }
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
